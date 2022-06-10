c755192f1c0071df493f532927cc104732b8d2c8
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2022-06-10 12:40:45 +0200

    release script

---
+++ scripts/release.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euxo
+
+version=$(cat "package.json" | jq -r .version)
+echo releasing "v$version..."
+
+git tag "v$version"
+git push origin "v$version"
+# https://cli.github.com/manual/gh_release_create
+gh release create "v$version"
+npm publish
