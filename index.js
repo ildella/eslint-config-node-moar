759c4ae4a5824099e37845e5a0f18e1bec40dd3a
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2024-02-15 19:41:35 +0000

    stricter and tests rules + configs imported here, setup to have this module export muliple configs and bump to 1.0

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-module.exports = require('.eslintrc.js')
+const core = require('./lib/eslint-moar-config-core')
+const stricter = require('./lib/eslint-moar-config-stricter')
+const test = require('./lib/eslint-moar-config-test')
+
+module.exports = {
+  configs: {
+    core,
+    stricter,
+    test,
+  },
+}
